07482d419ffeb992d3d40bc8a27dcae8eb835d9d
along2000 <alonglongtimeago0130@gmail.com>
2023-01-09 14:40:28 +0900

    バリデーションの設定
    バリデーションの設定

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -8,18 +9,12 @@ Bundler.require(*Rails.groups)
 
 module Furima38537
   class Application < Rails::Application
-    before_action :configure_permitted_parameters, if: :devise_controller?
+   
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    before_action :configre_permitted_parameters, if: :devise_controller?
-private
-  def configre_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
   end
-
+end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
-end
