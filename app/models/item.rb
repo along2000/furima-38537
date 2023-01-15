d815d25ee5d2cedc914fcb879967cb6e01a1ce81
along2000 <alonglongtimeago0130@gmail.com>
2023-01-15 12:43:40 +0900

    モデル単体テストの実施
    モデル単体テストの実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,9 +8,12 @@ class Item < ApplicationRecord
   belongs_to :delivery_charge
   belongs_to :delivery_source
   belongs_to :delivery_date
-  validates :user, :text, :name, :price, :image, presence: true
- 
-  validates :price, format: { with: /\A[0-9]+\z/ , message: "price half-width number "},
-  numericality: {greater_than: 299, less_than: 10000000, message: "price out of range"}
+  validates :name,
+  length:{ maximum: 40, message: "40文字までです" }
+  validates :text,
+  length:{ maximum: 1000, message: "1000文字までです" }
+  validates :user,:text, :name, :price, :image, presence: true
+  validates :price, format: { with: /\A[0-9]+\z/ },
+  numericality: {greater_than: 299, less_than: 10000000, message: "300から999999までの数字で入力してください"}
   validates :delivery_date_id, :delivery_charge_id, :delivery_source_id, :category_id, :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+      name                  { '15年前の切手' }
+      text                  { '15年前の切手です。実家の押し入れから出てきましたが、価値がわからないため出品します。' }
+      price                 { '10000' }
+      delivery_date_id      { '2' }
+      delivery_charge_id    { '2' }
+      delivery_source_id    { '2' }
+      category_id           { '2' }
+      status_id             { '2' }
+      association :user
   end
 end
