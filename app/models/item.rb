3cf369346f07710494153fe849dce331825e003a
along2000 <alonglongtimeago0130@gmail.com>
2023-01-15 18:29:55 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,8 +13,9 @@ class Item < ApplicationRecord
   validates :text,
             length: { maximum: 1000, message: '1000文字までです' }
   validates :text, :name, :price, :image, presence: true
-  validates :price, format: { with: /\A[0-9]+\z/ },
-                    numericality: { greater_than: 299, less_than: 10_000_000, message: '300から999999までの数字で入力してください' }
+  validates :price,
+            numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                            message: '300から999999までの数字で入力してください' }
   validates :delivery_date_id, :delivery_charge_id, :delivery_source_id, :category_id, :status_id,
             numericality: { other_than: 1, message: "can't be blank" }
 end
