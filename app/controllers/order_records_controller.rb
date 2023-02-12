5b8e42f52b6fa5937fa9216925083f7669f6a24c
along2000 <alonglongtimeago0130@gmail.com>
2023-02-12 21:40:09 +0900

    クレジットカード購入機能の実装

--- app/controllers/order_records_controller.rb
+++ app/controllers/order_records_controller.rb
@@ -1,9 +1,38 @@
 class OrderRecordsController < ApplicationController
+  before_action :item_set, only: [:index, :create]
+
   def index
-    @order_record_payment = OrderRecordPaymentItem.find(params[:item_id])
+    @order_record_payment = OrderRecordPayment.new
   end
 
   def create
-    
+    @order_record_payment = OrderRecordPayment.new(order_params)
+      if @order_record_payment.valid?
+        payment
+        @order_record_payment.save
+        return redirect_to root_path
+      else
+        render 'index'
+      end
   end
+
+  private
+
+  def item_set
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_record_payment).permit(:postcode, :delivery_source_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def payment
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
--- app/models/order_record_payment.rb
+++ app/models/order_record_payment.rb
@@ -1,13 +1,17 @@
 class OrderRecordPayment
   include ActiveModel::Model
-  attr_accessor :post_code, :delivery_source_id, :city, :block, :building, :phone_number, :order_record_id, :user_id, :item_id
+  attr_accessor :postcode, :delivery_source_id, :city, :block, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "郵便番号は「3桁ハイフン4桁」の半角文字列で入力してください"}
-    validates :user_id, :item_id, :city, :block, :order_record_id
-    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "電話番号は10桁から11桁の数字のみ入力してください"}
-  end
-  validates :delivery_source_id, numericality: {other_than: 0, message: "都道府県を選択してください"}
+    validates :user_id
+    validates :item_id
+    validates :city
+    validates :block
+    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "電話番号は10桁から11桁の数字のみ入力してください"}
+    validates :delivery_source_id, numericality: {other_than: 0, message: "都道府県を選択してください"}
+    validates :token
+ end
 
   def save
     order_record = OrderRecord.create(user_id: user_id, item_id: item_id)
